611ab6f80a4a0db3a53e67e9574eabb135448208
David Jaros <jdavid.cz@gmail.com>
2020-10-08 22:55:59 +0200

    tuning

--- finance/kalman.py
+++ finance/kalman.py
@@ -2,10 +2,13 @@ from pykalman import KalmanFilter
 import numpy as np
 from finance.alphavantage.sync import read_json
 
+NSAMPLES = 100
+NDIM = 4
+
 def read_data(samples=10, derivations=3, symbol='AAPL', field='4. close'):
 
     def append_derivatives(matrix, remaining_derivatives):
-        if remaining_derivatives >0:
+        if remaining_derivatives > 0:
             new_matrix = np.vstack([matrix, np.gradient(matrix[-1])])
             return append_derivatives(new_matrix, remaining_derivatives-1)
         else:
@@ -13,13 +16,15 @@ def read_data(samples=10, derivations=3, symbol='AAPL', field='4. close'):
 
     response = read_json(symbol)
 
-    sorted_keys = list(response.keys())[:samples] #TODO implement sorting, don't rely that data are always sorted
+    sorted_keys = reversed(list(response.keys())[:samples]) #TODO implement sorting, don't rely that data are always sorted
     data_1d = np.array([[response[key][field] for key in sorted_keys]], dtype=float)
-    return append_derivatives(data_1d, derivations)
+    return append_derivatives(data_1d, derivations).transpose()
 
 
+measurements = read_data(samples=NSAMPLES, derivations=NDIM-1)
 
+kf = KalmanFilter(n_dim_obs=NDIM, n_dim_state=NDIM)
+results = kf.em(measurements, n_iter=20).smooth(measurements)[0]
 
-p = read_data()
-
+#means, covariances = kf.filter(measurements)
 pass
