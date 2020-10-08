c86711ad5b3581c4a40a4392953734467583a436
David Jaros <jdavid.cz@gmail.com>
2020-10-09 00:17:50 +0200

    tuning

--- finance/kalman.py
+++ finance/kalman.py
@@ -3,7 +3,8 @@ import numpy as np
 from finance.alphavantage.sync import read_json
 
 NSAMPLES = 100
-NDIM = 4
+NDIM = 20
+SYMBOL = 'AAPL'
 
 
 def state_transition_matrix(n_dim_y, n_dim_x):
@@ -28,16 +29,22 @@ def read_data(samples=10, derivations=3, symbol='AAPL', field='4. close'):
 
     sorted_keys = reversed(list(response.keys())[:samples]) #TODO implement sorting, don't rely that data are always sorted
     data_1d = np.array([[response[key][field] for key in sorted_keys]], dtype=float)
+    print(f'latest date: {list(response.keys())[0]}')
     return append_derivatives(data_1d, derivations).transpose()
 
-
-measurements = read_data(samples=NSAMPLES, derivations=NDIM-1)
+measurements = read_data(samples=NSAMPLES, derivations=NDIM-1, symbol=SYMBOL)
 
 kf_smoothing = KalmanFilter(n_dim_obs=NDIM, n_dim_state=NDIM)
 results_smoothed = kf_smoothing.em(measurements, n_iter=20).smooth(measurements)[0]
 
-#means, covariances = kf.filter(measurements)
-pass
+F = state_transition_matrix(NDIM, NDIM)
+kf_designed = KalmanFilter(n_dim_obs=NDIM,
+                           n_dim_state=NDIM,
+                           transition_matrices=F).em(measurements, n_iter=20)
+(filtered_state_means, filtered_state_covariances) = kf_designed.filter(measurements)
 
-F=state_transition_matrix(5, 5)
-pass
+
+print(filtered_state_means[-1])
+print(filtered_state_covariances[-1])
+
+print(f'one day prediction for {SYMBOL}: {sum(filtered_state_means[-1])}')
